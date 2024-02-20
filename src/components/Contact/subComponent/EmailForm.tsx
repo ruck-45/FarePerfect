a02b3f540a8a450dd41753c37ad71640dc477c01
AnishMishra1 <124522187+AnishMishra1@users.noreply.github.com>
2024-02-20 15:32:21 +0530

    Mail (#16)
    * add disclaimer and refund
    
    * email configuration
    
    * email form validation and setup mail
    
    ---------
    
    Co-authored-by: AnishMishra1 <Anishmishra@gmail.com>

--- src/components/Contact/subComponent/EmailForm.tsx
+++ src/components/Contact/subComponent/EmailForm.tsx
@@ -2,17 +2,17 @@
 import { Input, Textarea, Button } from "@nextui-org/react";
 import { IoSend } from "react-icons/io5";
 import { useState, useRef, FormEvent } from "react";
-// import emailjs from "@emailjs/browser";
-// import toast, { Toaster, ToastPosition } from "react-hot-toast";
+import emailjs from "@emailjs/browser";
+import toast, { Toaster, ToastPosition } from "react-hot-toast";
 
 const emailRe: RegExp = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_-]+)(\.[a-zA-Z]{2,5}){1,2}$/;
-// const toastSetting: {
-//   position: ToastPosition;
-// } = { position: "top-center" };
+const toastSetting: {
+  position: ToastPosition;
+} = { position: "top-center" };
 
-// const formNotFill = (): string => toast.error("Please Fill The Form Correctly", toastSetting);
-// const emailSent = (): string => toast.success("Email Sent", toastSetting);
-// const emailNotSent = (): string => toast.error("Email Not Sent", toastSetting);
+const formNotFill = (): string => toast.error("Please Fill The Form Correctly", toastSetting);
+const emailSent = (): string => toast.success("Email Sent", toastSetting);
+const emailNotSent = (): string => toast.error("Email Not Sent", toastSetting);
 
 const EmailForm = () => {
   const form = useRef<HTMLFormElement>(null);
@@ -22,24 +22,30 @@ const EmailForm = () => {
 
   const [emailState, setEmailState] = useState<number>(-1);
   const [userNameState, setUserNameState] = useState<number>(-1);
+  const [state, setState] = useState(false);
 
-  //   const sendEmail = async () => {
-  //     try {
-  //       if (!emailValidity && userNameState > 0 && emailState > 0) {
-  //         await emailjs.sendForm(
-  //           `${process.env.REACT_APP_SERVICE_ID}`,
-  //           `${process.env.REACT_APP_TEMPLATE_ID}`,
-  //           form.current!,
-  //           `${process.env.REACT_APP_PUBLIC_KEY}`
-  //         );
-  //         emailSent();
-  //       } else {
-  //         formNotFill();
-  //       }
-  //     } catch (error) {
-  //       emailNotSent();
-  //     }
-  //   };
+  const sendEmail = async () => {
+    setState(true);
+
+    try {
+      if (!emailValidity && userNameState > 0 && emailState > 0) {
+        await emailjs.sendForm(
+          `${process.env.REACT_APP_SERVICE_ID}`,
+          `${process.env.REACT_APP_TEMPLATE_ID}`,
+          form.current!,
+          `${process.env.REACT_APP_PUBLIC_KEY}`
+        );
+        emailSent();
+        setState(false);
+      } else {
+        formNotFill();
+        setState(false);
+      }
+    } catch (error) {
+      emailNotSent();
+      setState(false);
+    }
+  };
 
   const checkEmail = (event: FormEvent<HTMLInputElement>) => {
     email.current = event.currentTarget.value;
@@ -58,22 +64,28 @@ const EmailForm = () => {
       <div>
         <h1 className="text-[2rem] text-black font-semibold font-['Kanit']">CONTACT US</h1>
       </div>
-      <form className="flex flex-col gap-[1rem] items-start w-[100%] grow" ref={form}>
+      <form className="flex flex-col gap-[1rem] items-start w-[100%] grow" ref={form} onSubmit={sendEmail}>
         <Input
           type="text"
           classNames={{ input: "text-black" }}
           label="Your Name"
           variant="bordered"
-          name="subject"
+          name="name"
           radius="none"
+          onChange={(event) => setUserNameState(event.currentTarget.value.length)}
+          errorMessage={userNameState === 0 ? "Please enter a valid Name" : ""}
+          isInvalid={userNameState === 0}
         />
         <Input
-          type="number"
+          type="email"
           classNames={{ input: "text-black" }}
-          label="Phone Number"
+          label="Email"
           variant="bordered"
-          name="subject"
+          name="email"
           radius="none"
+          onChange={checkEmail}
+          isInvalid={emailValidity}
+          errorMessage={emailValidity ? "Please enter a valid Email" : ""}
         />
         <Input
           type="text"
@@ -95,11 +107,12 @@ const EmailForm = () => {
           radius="none"
           className="w-[12rem] h-[3rem]  text-white bg-[#0a97b0]"
           endContent={<IoSend className="mt-[0.2rem]" />}
-          //   onClick={sendEmail}
+          onClick={sendEmail}
+          isLoading={state}
         >
           Send
         </Button>
-        {/* <Toaster /> */}
+        <Toaster />
       </form>
     </div>
   );
